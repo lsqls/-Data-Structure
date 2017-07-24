2ce3282670c879dc3844e0fb0069d1b7e8f966ae
king <764666894@qq.com>
2017-07-24 21:31:18 +0800

    链表

---
+++ 链表/未命名1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <malloc.h>
+using namespace std;
+typedef struct node* list;
+struct node{
+	int data;
+	list next;
+};
+int length(list L)
+{
+	list p=L;
+	int j=0;
+	while(p)
+	p=p->next;
+	j++;
+	return j;
+}
+list find(int K,list L) 
+{
+list  p=L;
+int i=1;
+while(p!=NULL&&i<K)
+{
+	p=p->next;
+	i++;
+}
+if(i==K)
+	return p;
+else
+	return NULL;
+}
+list Find(int x,list L)
+{
+	list p=L;
+	while(p->data!=x&&p!=NULL)
+	{
+	p=p->next;
+	}
+	return p;
+}
+void insert(int x,int i,list pt)
+{	int j=0;
+	list p,a;
+	if(i==1)
+	{
+	a=(list)malloc(sizeof(node));
+	a->data=x;
+	a->next=pt;
+	}		
+	p=find(i-1,p);
+	if(p==NULL)
+	{
+		cout<<"²ÎÊý´íÎó";
+	}
+else
+{
+	a=(list)malloc(sizeof(node));
+	a->data=x;
+	a->next=p->next;
+	p->next=a->next;
+}
+}
+	
+void Delete(int i,int list)
+{
+	
+}
+int main()
+{
+	return 0;
+}
+
